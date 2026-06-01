5d2c9e5eded2a86b6d3975c70f967a819f3d9c82
Saathwik Dasari <saathwik.dasari@gmail.com>
2026-06-01 18:56:59 +0530

    feat(parser): implement parser skeleton and basic let statement parsing
    - Create Parser struct tracking current and peek tokens
    
    - Implement main program parsing loop and token advancement
    
    - Add initial statement routing with support for let statement structure
    
    - Add unit test verifying successful parsing of variable bindings

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -51,6 +51,66 @@ impl Parser {
     }
 
     fn parse_let_statement(&mut self) -> Option<Statement> {
-        todo!();
+        let name = match &self.peek_token {
+            Token::Ident(ident_name) => ident_name.clone(),
+            _ => return None,
+        };
+
+        self.next_token();
+
+        if self.peek_token != Token::Assign {
+            return None;
+        }
+
+        self.next_token();
+        self.next_token();
+
+        // TODO: I have to parse the mathematical expression here.... Will do it later!
+
+        while self.current_token != Token::SemiColon && self.current_token != Token::EOF {
+            self.next_token();
+        }
+
+        Some(Statement::Let {
+            name,
+            value: Expression::IntegralLiteral(0),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic;
+
+    use super::*;
+    use crate::ast::Statement;
+    use crate::lexer::Lexer;
+
+    #[test]
+    fn test_let_statements() {
+        let input = String::from("let x = 10; let y = 20");
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+
+        // In the test input given earlier, we have given only 2 statements so we expect 2
+        assert_eq!(program.statements.len(), 2);
+
+        // After checking no. of statements now we check branches
+        let stmt1 = &program.statements[0];
+        if let Statement::Let { name, value: _ } = stmt1 {
+            assert_eq!(name, "x");
+        } else {
+            panic!("Expected Let, got: {:?}", stmt1);
+        }
+
+        // Now let's verify the second branch too
+        let stmt2 = &program.statements[1];
+        if let Statement::Let { name, value: _ } = stmt2 {
+            assert_eq!(name, "y");
+        } else {
+            panic!("Expected Let, got: {:?}", stmt2);
+        }
     }
 }
